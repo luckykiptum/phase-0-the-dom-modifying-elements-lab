d37f4a52aa187370ac53c29f2785b22303c82d66
lucky <luckykiptum@gmail.com>
2024-04-08 20:27:54 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+main.remove()
+
+const newHeader=document.createElement('h1')
+newHeader.id = 'victory';
+const name ='Jim Hariclint'
+newHeader.textContent=`${name} is the champion`;
+
+body.appendChild(newHeader);
